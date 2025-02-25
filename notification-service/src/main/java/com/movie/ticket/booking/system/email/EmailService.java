d8b4f59f463c8ae6989a8ca67780e7e4f918d6b4
manasa-bhagwat <maansa.bhagwat@gmail.com>
2025-02-25 23:29:09 +0530

    added notification-service. working.

---
+++ notification-service/src/main/java/com/movie/ticket/booking/system/email/EmailService.java
@@ -0,0 +1,30 @@
+package com.movie.ticket.booking.system.email;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class EmailService {
+
+    private final JavaMailSender mailSender;
+
+    public void sendEmail(String to, String subject, String text) {
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setTo(to);
+            message.setSubject(subject);
+            message.setText(text);
+            mailSender.send(message);
+            log.info("Email sent successfully to {}", to);
+
+        } catch (Exception e) {
+            log.error("Failed to send email to {}: {}", to, e.getMessage());
+        }
+    }
+
+}
--- notification-service/src/main/java/com/movie/ticket/booking/system/kafka/listener/NotificationServiceKafkaListener.java
+++ notification-service/src/main/java/com/movie/ticket/booking/system/kafka/listener/NotificationServiceKafkaListener.java
@@ -1,6 +1,7 @@
 package com.movie.ticket.booking.system.kafka.listener;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.movie.ticket.booking.system.email.EmailService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -12,14 +13,20 @@ import org.springframework.stereotype.Service;
 public class NotificationServiceKafkaListener {
 
     private final ObjectMapper objectMapper;
+    private final EmailService emailService;
 
     @KafkaListener(topics = "payment-response", groupId = "payment-response-group2")
     public void pullBookingDetailsFromPaymentResponseTopic(String bookingsJson) {
         log.info("Received booking details {} from payment-response topic", bookingsJson);
         try {
             //send email logic
+            String recipientEmail = "maansaflaunts@gmail.com";
+            String subject = "Booking confirmation";
+            String messageBody = "Your booking is confirmed! Details: " + bookingsJson;
+
+            emailService.sendEmail(recipientEmail, subject, messageBody);
         } catch (Exception e) {
-            log.info("Error while sending email for booking details {} ", bookingsJson);
+            log.error("Error while sending email for booking details {} ", bookingsJson, e);
         }
     }
 }
